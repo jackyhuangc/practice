0c54ccbed55480491e78efcb19be84612d89ab87
huangchao <huangchao@kuainiugroup.com>
2019-09-11 19:35:52 +0800

    apollo

---
+++ src/main/java/com/jacky/javassist/T_Base.java
@@ -0,0 +1,32 @@
+package com.jacky.javassist;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * 请输入描述
+ *
+ * @author Jacky
+ * @date 2019/9/10 6:29 PM
+ */
+
+public class T_Base {
+    public static void main(String[] args) {
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+
+        String s = name.split("@")[0];
+        //打印当前Pid
+        System.out.println("pid:" + s);
+        while (true) {
+            try {
+                Thread.sleep(5000L);
+            } catch (Exception e) {
+                break;
+            }
+            process();
+        }
+    }
+
+    public static void process() {
+        System.out.println("process");
+    }
+}
--- src/main/java/com/jacky/practice/T_Proxy.java
+++ src/main/java/com/jacky/practice/T_Proxy.java
@@ -1,5 +1,7 @@
 package com.jacky.practice;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -16,6 +18,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javassist.*;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
@@ -35,162 +42,201 @@ import org.springframework.web.context.support.HttpRequestHandlerServlet;
  */
 public class T_Proxy {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		JDKProxy jdkProxy = new JDKProxy();
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        JDKProxy jdkProxy = new JDKProxy();
 
-		// 相当于接口
-		UserManager ujdk = (UserManager) jdkProxy.newProxy(new UserManagerImpl());
-		ujdk.addUser("123", "12");
+        // 相当于接口
+        UserManager ujdk = (UserManager) jdkProxy.newProxy(new UserManagerImpl());
+        ujdk.addUser("123", "12");
 
-		// 相当于继承
-		UserManager ucglib = (UserManager) new CGLibProxy().createProxyObject(new UserManagerImpl());
-		ucglib.addUser("111", "11");
+        // 相当于继承
+        UserManager ucglib = (UserManager) new CGLibProxy().createProxyObject(new UserManagerImpl());
+        ucglib.addUser("111", "11");
 
-		UserManager u = new UserManager() {
+        UserManager u = new UserManager() {
 
-			@Override
-			public void addUser(String id, String password) {
-				// TODO Auto-generated method stub
-				System.out.println("xxxxxxxxxxxx" + id + password);
-			}
+            @Override
+            public void addUser(String id, String password) {
+                // TODO Auto-generated method stub
+                System.out.println("xxxxxxxxxxxx" + id + password);
+            }
 
-			@Override
-			public void delUser(String id) {
-				// TODO Auto-generated method stub
+            @Override
+            public void delUser(String id) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-		};
+        };
 
-		u.addUser("xxx", "bbb");
-	}
+        u.addUser("xxx", "bbb");
+    }
+
+    @Test
+    public void test() throws IOException, CannotCompileException, NotFoundException, IllegalAccessException, InstantiationException {
+		/*
+
+		Class.forName
+		@Retention
+				ObjectOutputStream
+		Dubbo 底层  客户端负载
+
+		volatile
+		synchronized  ObjectMonitor wait(),notify()
+
+		jdk proxy   ProxyGenerator
+
+		ASM-cglib
+		instrumentation-agent  transForm
+		ASM-javassit
+
+		*/
+
+        ClassPool cp = ClassPool.getDefault();
+        CtClass cc = cp.get("com.jacky.practice.Base");
+        CtMethod m = cc.getDeclaredMethod("process");
+        m.insertBefore("{ System.out.println(\"start\"); }");
+        m.insertAfter("{ System.out.println(\"end\"); }");
+        Class c = cc.toClass();
+        cc.writeFile("/Users/huangchao/IdeaProjects/practice/target/classes/com/jacky/practice/Basexxx");
+        Base h = (Base) c.newInstance();
+        h.process();
+
+    }
 }
 
 class TimerFilter implements Filter {
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
 
-		Servlet x;
-		HttpServletRequest x1;
-		HttpServlet h1;
+        Servlet x;
+        HttpServletRequest x1;
+        HttpServlet h1;
 
-	}
+    }
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 }
 
 // HttpServlet类实现Servlet接口
 class HelloServlet extends HttpServlet {
 
-	// HTTP协议中的请求和响应就是对应了 HttpServletRequest 和 HttpServletResponse 这两个接口
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("Hello World");
-	}
+    // HTTP协议中的请求和响应就是对应了 HttpServletRequest 和 HttpServletResponse 这两个接口
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Hello World");
+    }
 }
 
 // Servlet 规范里还有另外一个非常重要而且非常有用的接口那就是 Filter 过滤器。
 class HelloFilter implements Filter {
 
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		System.out.println("Filter init...");
-	}
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        System.out.println("Filter init...");
+    }
 
-	// Filter不是一个servlet，它不能产生一个response，但是他能在request到达servlet之前预先处理request，也可以在响应离开servlet时处理response。
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		System.out.println("拦截 URI=" + request.getRequestURI());
-		chain.doFilter(req, res);
-	}
+    // Filter不是一个servlet，它不能产生一个response，但是他能在request到达servlet之前预先处理request，也可以在响应离开servlet时处理response。
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        System.out.println("拦截 URI=" + request.getRequestURI());
+        chain.doFilter(req, res);
+    }
 
-	@Override
-	public void destroy() {
-		System.out.println("Filter destroy..");
-	}
+    @Override
+    public void destroy() {
+        System.out.println("Filter destroy..");
+    }
 }
 
 interface UserManager {
-	void addUser(String id, String password);
+    void addUser(String id, String password);
 
-	@Autowired()
-	void delUser(String id);
+    @Autowired()
+    void delUser(String id);
 }
 
 class UserManagerImpl implements UserManager {
 
-	@Override
-	public void addUser(String id, String password) {
-		// TODO Auto-generated method stub
-		System.out.println("调用了addUser" + id + "***" + password);
-	}
+    @Override
+    public void addUser(String id, String password) {
+        // TODO Auto-generated method stub
+        System.out.println("调用了addUser" + id + "***" + password);
+    }
 
-	@Override
-	public void delUser(String id) {
-		// TODO Auto-generated method stub
-		System.out.println("调用了addUser" + id);
-	}
+    @Override
+    public void delUser(String id) {
+        // TODO Auto-generated method stub
+        System.out.println("调用了addUser" + id);
+    }
 }
 
 // JDK代理
 class JDKProxy implements InvocationHandler {
-	private Object targetObject;
+    private Object targetObject;
 
-	public Object newProxy(Object targetObject) {
-		// TODO Auto-generated method stub
-		this.targetObject = targetObject;
+    public Object newProxy(Object targetObject) {
+        // TODO Auto-generated method stub
+        this.targetObject = targetObject;
 
-		return Proxy.newProxyInstance(targetObject.getClass().getClassLoader(), targetObject.getClass().getInterfaces(),
-				this);
-	}
+        return Proxy.newProxyInstance(targetObject.getClass().getClassLoader(), targetObject.getClass().getInterfaces(),
+                this);
+    }
 
-	@Override
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-		Object ret = null;
+        Object ret = null;
 
-		// 添加切面逻辑（advice），此处是在目标类代码执行之前
-		ret = method.invoke(targetObject, args);
-		// 添加切面逻辑（advice），此处是在目标类代码执行之后
-		return ret;
-	}
+        // 添加切面逻辑（advice），此处是在目标类代码执行之前
+        ret = method.invoke(targetObject, args);
+        // 添加切面逻辑（advice），此处是在目标类代码执行之后
+        return ret;
+    }
+}
+
+
+class Base {
+    public void process() {
+        System.out.println("process");
+    }
 }
 
 class CGLibProxy implements MethodInterceptor {
-	Object targetObject;
+    Object targetObject;
 
-	public Object createProxyObject(Object obj) {
-		this.targetObject = obj;
-		Enhancer enhaner = new Enhancer();
-		enhaner.setSuperclass(obj.getClass());
-		enhaner.setCallback(this);
+    public Object createProxyObject(Object obj) {
+        this.targetObject = obj;
+        Enhancer enhaner = new Enhancer();
+        enhaner.setSuperclass(obj.getClass());
+        enhaner.setCallback(this);
 
-		Object proxyObj = enhaner.create();
-		return proxyObj;
-	}
+        Object proxyObj = enhaner.create();
+        return proxyObj;
+    }
 
-	@Override
-	public Object intercept(Object targetObject, Method method, Object[] args, MethodProxy arg3) throws Throwable {
+    @Override
+    public Object intercept(Object targetObject, Method method, Object[] args, MethodProxy arg3) throws Throwable {
 
-		// 添加切面逻辑（advice），此处是在目标类代码执行之前
-		Object ret = method.invoke(this.targetObject, args);
-		// 添加切面逻辑（advice），此处是在目标类代码执行之后
-		return ret;
-	}
+        // 添加切面逻辑（advice），此处是在目标类代码执行之前
+        Object ret = method.invoke(this.targetObject, args);
+        // 添加切面逻辑（advice），此处是在目标类代码执行之后
+        return ret;
+    }
 }
