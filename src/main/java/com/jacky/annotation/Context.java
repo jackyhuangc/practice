f892780f48e51498e3388095e34740a40170eebb
huangchao <huangchao@kuainiugroup.com>
2019-09-30 15:48:10 +0800

    apollo

--- src/main/java/com/jacky/annotation/Context.java
+++ src/main/java/com/jacky/annotation/Context.java
@@ -16,9 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2019/9/12 7:29 PM
  */
 @ComponentScan("com.jacky.annotation")
-//@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy // 启用AOP机制，同时需要增加@Aspect配置
 @EnableAspectJAutoProxy(proxyTargetClass = true)// 强制使用cglib方式生成代理类
-@EnableTransactionManagement
+@EnableTransactionManagement // 启用事务机制，同时需要使用@Transactional注解
 public class Context {
 
     @Autowired
@@ -28,6 +28,13 @@ public class Context {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Context.class);
+        /**
+         * AnnotationConfigApplicationContext构造函数中，经过了register和refresh两步
+         * 1.register扫描指定包下的@Component注解，并将bean定义注册到beanDefinitionMap中。
+         * 2.refresh将bean定义，通过beanFactory实例化后缓存到singletonObjects中。
+         *
+         * 因此在beanFactory中的beanDefinitionMap和singletonObjects已经不为空
+         */
 
         //configApplicationContext.scan("com.jacky.annotation");
         //configApplicationContext.refresh();
@@ -42,9 +49,12 @@ public class Context {
         // 如果启用了事务（@EnableTransactionManagement、@Transactional），且如果是采用JDK动态代理，则肯定是实现的接口，只能用接口转换
         IUserService transcationUserService = context.getBean("transactionUserService", IUserService.class);
 
+        // 这个是标准的原始pojo对象
+        TestService testService = context.getBean(TestService.class);
+
         String name = userService1.getUser("xxxx");
 
-        /***
+        /**
          * 1.启用了事务（@EnableTransactionManagement、@Transactional），实现了接口，将使用Jdk动态代理生成代理对象；getBean时只能用接口转换
          * 2.启用了事务（@EnableTransactionManagement、@Transactional），实现了接口，强制指定@EnableTransactionManagement(proxyTargetClass = true)或@EnableAspectJAutoProxy(proxyTargetClass=true)，将使用Cglib动态代理生成代理对象；getBean时可用接口和对象转换
          * 3.启用了事务（@EnableTransactionManagement、@Transactional），没有实现任何接口，将使用Cglib动态代理生成代理对象；getBean时可用对象转换
@@ -55,6 +65,6 @@ public class Context {
          *
          *
          * PS:最终的测试结果大致是这样的，在1.6和1.7的时候，JDK动态代理的速度要比CGLib动态代理的速度要慢，但是并没有教科书上的10倍差距，在JDK1.8的时候，JDK动态代理的速度已经比CGLib动态代理的速度快很多了，希望小伙伴在遇到这个问题的时候能够有的放矢！
-         * ***/
+         */
     }
 }
