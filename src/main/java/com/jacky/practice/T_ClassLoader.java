3b94405a6ac3c393eeb6e704c28fddfb2139eeb5
huangchao <huangchao@kuainiugroup.com>
2019-10-16 18:28:57 +0800

    清理配置文件

--- src/main/java/com/jacky/practice/T_ClassLoader.java
+++ src/main/java/com/jacky/practice/T_ClassLoader.java
@@ -1,5 +1,7 @@
 package com.jacky.practice;
 
+import sun.misc.Launcher;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -15,23 +17,45 @@ import java.util.List;
  */
 public class T_ClassLoader {
     public static void main(String[] args) {
-
+        // test(new T_ClassLoader());
 //        System.out.println(ClassLoader.getSystemClassLoader());
 //        String classPath = System.getProperty("java.class.path");
 //        for (String path : classPath.split(";")) {
 //            System.out.println(path);
 //        }
 
+
+        Object obj = new Object();
+        sun.security.ec.SunEC obj1 = new sun.security.ec.SunEC();
+        T_ClassLoader obj2 = new T_ClassLoader();
+        // 这是启动类
+        System.out.println(String.format("启动类%s,扩展类%s,应用类%s", isJdkClass(obj.getClass()), isExtClass(obj.getClass()), isAppClass(obj.getClass())));
+
+        // 这是扩展类
+        System.out.println(String.format("启动类%s,扩展类%s,应用类%s", isJdkClass(obj1.getClass()), isExtClass(obj1.getClass()), isAppClass(obj1.getClass())));
+
+        // 这是应用类
+        System.out.println(String.format("启动类%s,扩展类%s,应用类%s", isJdkClass(obj2.getClass()), isExtClass(obj2.getClass()), isAppClass(obj2.getClass())));
+
         ClassLoader cls = T_ClassLoader.class.getClassLoader();
 
         //System.out.println(cls.toString());
         while (cls != null) {
 
-            System.out.println(cls.toString());
+            System.out.println(cls);
             cls = cls.getParent();
         }
-
         System.out.println(cls);
+
+        System.out.println(ClassLoader.getSystemClassLoader()); // AppClassLoader
+        System.out.println(Launcher.getLauncher().getClassLoader()); // AppClassLoader
+
+        // ClassLoader和Launcher类都是由启动类加载器加载到内存中的。
+        // Launcher类属于sun.misc包，这个包属于charsets.jar包下，从上面的输出结果中，可以看到这个包是由启动类加载器加载的；
+        // 而ClassLoader类是位于java.lang包下，位于resources.jar包下，同样也是由启动类加载器加载的
+        System.out.println(ClassLoader.class.getClassLoader()); // null
+        System.out.println(Launcher.class.getClassLoader()); // null
+
     }
 
     private static void test(Object obj) {
@@ -40,11 +64,19 @@ public class T_ClassLoader {
             return;
         }
 
+        // 上下文类加载为系统类加载器，可以通过ClassLoader.getSystemClassLoader()或Launcher.getLauncher().getClassLoader()取到
+        if (obj.getClass().getClassLoader() == ClassLoader.getSystemClassLoader()) {
+            System.out.println("这是由App ClassLoader加载加载的对象");
+        }
+
         List<Field> fields = getAllFields(obj.getClass());
         for (Field field : fields) {
             Object value = getFieldValue(field.getName(), obj);
 
             ClassLoader classLoader = field.getType().getClassLoader();
+            if (classLoader == Launcher.getLauncher().getClassLoader()) {
+
+            }
             if (!isJdkClass(field.getType())) {
 
                 // 可以是ExtClassLoader 加载的对象
@@ -94,6 +126,14 @@ public class T_ClassLoader {
 
     protected static Boolean isJdkClass(Class<?> clazz) {
         return clazz != null && clazz.getClassLoader() == null;
+    }
+
+    protected static Boolean isExtClass(Class<?> clazz) {
+        return !isJdkClass(clazz) && !isAppClass(clazz);
+    }
+
+    protected static Boolean isAppClass(Class<?> clazz) {
+        return clazz.getClassLoader() == ClassLoader.getSystemClassLoader();
     }
 }
 
