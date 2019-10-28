089b695e865a6aafc1ebac6342ff273db5645e64
huangchao <huangchao@kuainiugroup.com>
2019-10-28 19:27:57 +0800

    清理配置文件

--- src/main/java/com/jacky/dubbo/Consumer.java
+++ src/main/java/com/jacky/dubbo/Consumer.java
@@ -1,10 +1,39 @@
 package com.jacky.dubbo;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.jacky.dubbo.service.Circle;
+import com.jacky.dubbo.service.Shape;
+
 /**
- * 请输入描述
+ * 使用广播方式的dubbo消费端
  *
  * @author Jacky
  * @date 2019/10/24 11:25 AM
  */
 public class Consumer {
+
+    public Object getRemoteCall(String url) {
+        ReferenceConfig referenceConfig = new ReferenceConfig();
+
+        ApplicationConfig applicationConfig = new ApplicationConfig("consumer");
+        referenceConfig.setApplication(applicationConfig);
+
+        // 使用multicast广播方式作为注册中心 订阅服务
+        RegistryConfig registryConfig = new RegistryConfig("multicast://224.1.2.3:1234");
+        referenceConfig.setRegistry(registryConfig);
+
+        referenceConfig.setInterface(Shape.class);
+
+        referenceConfig.setLoadbalance("roundrobin");
+        return referenceConfig.get();
+    }
+
+    public static void main(String[] args) {
+        Consumer consumer = new Consumer();
+        Shape shape = (Shape) consumer.getRemoteCall(null);
+
+        shape.draw();
+    }
 }
--- src/main/java/com/jacky/dubbo/Provider.java
+++ src/main/java/com/jacky/dubbo/Provider.java
@@ -1,10 +1,52 @@
 package com.jacky.dubbo;
 
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
+import com.jacky.dubbo.service.Circle;
+import com.jacky.dubbo.service.Shape;
+
+import java.io.IOException;
+
 /**
- * 请输入描述
+ * 使用广播方式的dubbo服务端
  *
  * @author Jacky
  * @date 2019/10/24 11:25 AM
  */
 public class Provider {
+
+    public void start(int port) {
+        ServiceConfig serviceConfig = new ServiceConfig();
+        ApplicationConfig applicationConfig = new ApplicationConfig("provider");
+        serviceConfig.setApplication(applicationConfig);
+
+        ProtocolConfig protocolConfig = new ProtocolConfig("dubbo", port);
+        serviceConfig.setProtocol(protocolConfig);
+
+        // 使用multicast广播方式作为注册中心 暴露服务
+        RegistryConfig registryConfig = new RegistryConfig("multicast://224.1.2.3:1234");
+
+        //<!-- 标准使用zookeeper注册中心暴露服务地址 -->
+        //<dubbo:registry address="zookeeper://127.0.0.1:2181" />
+
+        serviceConfig.setRegistry(registryConfig);
+
+        serviceConfig.setInterface(Shape.class);
+        serviceConfig.setRef(new Circle());
+
+        // 暴露服务
+        serviceConfig.export();
+
+        port = ((URL) serviceConfig.getExportedUrls().get(0)).getPort();
+        System.out.println(String.format("服务启动：%s", port));
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Provider().start(-1);
+
+        System.in.read();
+    }
 }
