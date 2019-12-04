e41c479a10f2c79094b118235cf06b10acbd904f
huangchao <huangchao@kuainiugroup.com>
2019-12-04 15:41:11 +0800

    ABA问题

---
+++ src/main/java/com/jacky/cas/T_CAS.java
@@ -0,0 +1,134 @@
+package com.jacky.cas;
+
+
+import com.jacky.common.util.LogUtil;
+import com.jacky.rpc.dto.UserDto;
+import lombok.Data;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * 请输入描述
+ *
+ * @author Jacky
+ * @date 2019/12/4 2:43 PM
+ */
+public class T_CAS {
+
+    static UserDto user = new UserDto();
+
+    static {
+        user.setName("张三");
+        user.setAge(10);
+    }
+
+    static AtomicReference<UserDto> atomicReference = new AtomicReference<>(user);
+
+    // 更类似数据库的乐观锁，时间戳版本号的概念
+    static AtomicStampedReference<UserDto> atomicStampedReference = new AtomicStampedReference<>(user, 0);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(String.format("**************验证ABA问题的产生****************"));
+
+        TimeUnit.SECONDS.sleep(1);
+
+        new Thread(() -> {
+            // 1.线程1和线程2同时取到了【张三】
+            UserDto zhangsan = atomicReference.get();
+
+            try {
+                // 2.线程1只休眠1秒，立即执行修改逻辑
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 3.线程1先修改
+            UserDto lisi = new UserDto();
+            lisi.setName("李四");
+            lisi.setAge(11);
+            boolean blOk1 = atomicReference.compareAndSet(zhangsan, lisi);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s", Thread.currentThread().getName(), zhangsan.getName(), lisi.getName(), blOk1));
+            boolean blOk2 = atomicReference.compareAndSet(lisi, zhangsan);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s", Thread.currentThread().getName(), lisi.getName(), zhangsan.getName(), blOk2));
+        }, "线程1").start();
+
+        new Thread(() -> {
+            // 1.线程1和线程2同时取到了【张三】
+            UserDto zhangsan = atomicReference.get();
+
+            try {
+
+                // 2.线程2休眠2秒，比线程1延迟执行修改逻辑
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 3.线程2后修改，虽然能正常修改，但是过程已经被线程1修改多次，也就是ABA问题
+            UserDto lisi = new UserDto();
+            lisi.setName("李四");
+            lisi.setAge(11);
+            boolean blOk1 = atomicReference.compareAndSet(zhangsan, lisi);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s", Thread.currentThread().getName(), zhangsan.getName(), lisi.getName(), blOk1));
+            boolean blOk2 = atomicReference.compareAndSet(lisi, zhangsan);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s", Thread.currentThread().getName(), lisi.getName(), zhangsan.getName(), blOk2));
+        }, "线程2").start();
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(String.format("**************验证ABA问题的解决****************"));
+
+        new Thread(() -> {
+            // 1.线程3和线程4同时取到了【张三】
+            UserDto zhangsan = atomicStampedReference.getReference();
+            // 版本号原子引用，增加了版本号机制，更类似数据库的乐观锁，时间戳版本号的概念
+            int stamp = atomicStampedReference.getStamp();
+            LogUtil.info(String.format("%s第一次取到【%s】，版本【%s】", Thread.currentThread().getName(), zhangsan.getName(), stamp));
+
+            try {
+                // 2.线程1只休眠1秒，立即执行修改逻辑
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 3.线程1先修改
+            UserDto lisi = new UserDto();
+            lisi.setName("李四");
+            lisi.setAge(11);
+            boolean blOk1 = atomicStampedReference.compareAndSet(zhangsan, lisi, stamp, ++stamp);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s，版本变更为【%s】", Thread.currentThread().getName(), zhangsan.getName(), lisi.getName(), blOk1, atomicStampedReference.getStamp()));
+            boolean blOk2 = atomicStampedReference.compareAndSet(lisi, zhangsan, stamp, ++stamp);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s，版本变更为【%s】", Thread.currentThread().getName(), lisi.getName(), zhangsan.getName(), blOk2, atomicStampedReference.getStamp()));
+        }, "线程3").start();
+
+        new Thread(() -> {
+            // 1.线程3和线程4同时取到了【张三】
+            UserDto zhangsan = atomicStampedReference.getReference();
+            // 版本号原子引用，增加了版本号机制
+            int stamp = atomicStampedReference.getStamp();
+            LogUtil.info(String.format("%s第一次取到【%s】，版本【%s】", Thread.currentThread().getName(), zhangsan.getName(), stamp));
+
+            try {
+
+                // 2.线程2休眠2秒，比线程1延迟执行修改逻辑
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 3.线程2后修改，虽然能正常修改，但是过程已经被线程1修改多次，也就是ABA问题
+            UserDto lisi = new UserDto();
+            lisi.setName("李四");
+            lisi.setAge(11);
+
+            boolean blOk1 = atomicStampedReference.compareAndSet(zhangsan, lisi, stamp, ++stamp);
+            LogUtil.info(String.format("%s修改【%s】为【%s】：%s，期望版本【%s】，实际版本【%s】", Thread.currentThread().getName(), zhangsan.getName(), lisi.getName(), blOk1, stamp, atomicStampedReference.getStamp()));
+
+        }, "线程4").start();
+    }
+}
--- src/main/java/com/jacky/practice/T_SuperExtends.java
+++ src/main/java/com/jacky/practice/T_SuperExtends.java
@@ -72,22 +72,27 @@ public class T_SuperExtends {
          * 如果既要存又要取，那么就不要使用任何通配符
          */
         // 第一、频繁往外读取内容的，适合用<? extends T>
-        // 因为编译器只知道fruits是Fruit某个子类的List，但并不知道这个子类具体是什么类，为了类型安全，只好阻止向其中加入任何子类
+        // List<? extends Frut> 表示 “具有任何从Fruit继承类型的列表”，【*****编译器无法确定List所持有的类型*****】，所以无法安全的向其中添加对象，会造成数据丢失。可以添加null,因为null 可以表示任何类型。所以List 的add 方法不能添加任何有意义的元素，但是可以接受现有的子类型List<Apple> 赋值。
         List<? extends Self> listExtends = new ArrayList<>();
         List<? extends Self> listExtends1 = new ArrayList<Son>();
         List<? extends Self> listExtends2 = new ArrayList<Baby>();
-        //listExtends.add(new Son());
+        //listExtends2.add(new Son());// Baby有的,Son不一定有,Super也不一定有，所以干脆不支持
+
         // 编译器知道它总是 Self或其其继承的超类
-        Son son = (Son) listExtends1.get(0);
+        // 由于，其中放置是从Self中继承的类型，所以可以安全地取出Self类型，至少List中的对象数据不会丢失，可以明确转换出来。
+        //Son son = (Son) listExtends1.get(0);
 
         // 第二、经常往里插入的，适合用<? super T>。
+        // List<? super Fruit> 表示“具有任何Fruit超类型的列表”，【*****列表的类型至少是一个 Fruit 类型******】，因此可以安全的向其中添加Fruit 及其子类型，不会有任何数据丢失。
         List<? super Self> listSuper = new ArrayList<>();
         List<? super Self> listSuper1 = new ArrayList<Super>();
         listSuper.add(new Son());
-        listSuper1.add(new Son());
+        listSuper1.add(new Son());// Self/Super有的，Son都有
 
         // 编译器在不知道这个超类具体是什么类，只能返回Object对象，因为Object是任何Java类的最终祖先类
-        //Object super1 = listSuper.get(0);
+        // 由于List<? super Self>中的类型可能是任何Self的超类型，无法赋值为Self的子类型Son，数据不明确，转换已丢失.
+        //Self son1= (Self)listSuper.get(0);
+        Object super1 = listSuper.get(0);
 
         /**
          * extends 可用于的返回类型限定，不能用于参数类型限定。
